8eb3cb8bc5fdbfde39bdf2cb00d475fc37f5e4a6
laskarj <koks.adekvat@gmail.com>
2024-01-31 20:12:17 +0100

    Added management to users account:
    Routs:
    api/users/me/ < GET, PUT/PUTCH
    
    Detail:
    - Only authorised users have access to the path.

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from users.views import CreateUserView
+from users.views import CreateUserView, ManageUserView
 
 app_name = "users"
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path("", CreateUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
