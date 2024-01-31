24b58f49bf537af680094415775c93936557230f
laskarj <koks.adekvat@gmail.com>
2024-01-31 19:15:28 +0100

    Rename app user to users, change path api/users/register > api/users/

--- library_service/urls.py
+++ library_service/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/users/", include("user.urls", namespace="user")),
+    path("api/users/", include("users.urls", namespace="users")),
 ]
