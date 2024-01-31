acd14cde1c265f23ef4f60b811fa41e036a453c2
laskarj <koks.adekvat@gmail.com>
2024-01-31 22:51:30 +0100

    Code refactoring

--- library_service/settings.py
+++ library_service/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "users",
-    'rest_framework_simplejwt',
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -94,19 +94,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": (
-            "django.contrib.auth.password_validation"
-            ".UserAttributeSimilarityValidator"
-        ),
+        "NAME": "django.contrib.auth.password_validation"
+                ".UserAttributeSimilarityValidator",
     },
     {
-        "NAME": ("django.contrib.auth.password_validation" ".MinimumLengthValidator"),
+        "NAME": "django.contrib.auth.password_validation"
+                ".MinimumLengthValidator",
     },
     {
-        "NAME": ("django.contrib.auth.password_validation" ".CommonPasswordValidator"),
+        "NAME": "django.contrib.auth.password_validation"
+                ".CommonPasswordValidator",
     },
     {
-        "NAME": ("django.contrib.auth.password_validation" ".NumericPasswordValidator"),
+        "NAME": "django.contrib.auth.password_validation"
+                ".NumericPasswordValidator",
     },
 ]
 
