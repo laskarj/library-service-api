a3589148eb600dfc1ab455c75c102b5d8bb6ad1a
laskarj <koks.adekvat@gmail.com>
2024-02-01 13:43:35 +0100

    Auth header name changed Authorization > Authorize

--- library_service/settings.py
+++ library_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -139,4 +140,13 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
+}
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZE",
 }
