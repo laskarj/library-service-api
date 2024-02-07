bf387e08cca7e8b4f73148b4f119808d744af1f4
laskarj <koks.adekvat@gmail.com>
2024-02-07 18:31:45 +0100

    Remove InventorySerializer

--- books/views.py
+++ books/views.py
@@ -3,18 +3,11 @@ from rest_framework.serializers import ModelSerializer
 
 from books.permissions import IsAdminUserOrReadOnly
 from books.models import Book
-from books.serializers import (
-    BookSerializer,
-    InventorySerializer,
-)
+from books.serializers import BookSerializer
+
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = (IsAdminUserOrReadOnly, )
-
-    def get_serializer_class(self) -> ModelSerializer:
-        if self.action in ("put", "patch"):
-            return InventorySerializer
-        return BookSerializer
