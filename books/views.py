061d447a61a1181bbdaad087d2b017d92d02640b
laskarj <koks.adekvat@gmail.com>
2024-02-07 18:43:57 +0100

    Remove blank line

--- books/views.py
+++ books/views.py
@@ -6,7 +6,6 @@ from books.models import Book
 from books.serializers import BookSerializer
 
 
-
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
