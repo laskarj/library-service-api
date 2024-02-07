d11d137a8dd245b3e351dff550c151526397e81c
laskarj <koks.adekvat@gmail.com>
2024-02-07 12:38:43 +0100

    Docstring was added

--- books/models.py
+++ books/models.py
@@ -3,6 +3,19 @@ from django.db import models
 
 
 class Book(models.Model):
+    """
+    Model representing a book.
+
+    Attributes:
+        title (str): The title of the book.
+        author (str): The author of the book.
+        cover (str): The type of cover of the book (choices: "HARD", "SOFT").
+        inventory (int): The number of copies of the book available in the
+        library (must be a non-negative integer).
+        daily_fee (Decimal): The daily fee for borrowing the book in USD
+        (must be a non-negative decimal with up to 2 decimal places).
+    """
+
     COVER_CHOICES = (
         ("HARD", "Hard cover"),
         ("SOFT", "Soft cover"),
@@ -13,7 +26,5 @@ class Book(models.Model):
     cover = models.CharField(max_length=16, choices=COVER_CHOICES)
     inventory = models.PositiveIntegerField(validators=[MinValueValidator(0)])
     daily_fee = models.DecimalField(
-        max_digits=8,
-        decimal_places=2,
-        validators=[MinValueValidator(0)]
+        max_digits=8, decimal_places=2, validators=[MinValueValidator(0)]
     )
