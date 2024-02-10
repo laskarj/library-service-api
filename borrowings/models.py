8330e549db87f3be1cd9b40a3d9a7cbdf1568e32
laskarj <koks.adekvat@gmail.com>
2024-02-10 14:03:01 +0100

    Borrowing actual_return_date field configured

--- borrowings/models.py
+++ borrowings/models.py
@@ -7,7 +7,7 @@ from books.models import Book
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
     book = models.ForeignKey(
         Book,
         on_delete=models.CASCADE,
